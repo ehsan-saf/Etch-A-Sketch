59277f915e1c5af1d1b83e840adcce9d8bfd36eb
M.Ehsan <124778596+ehsan-saf@users.noreply.github.com>
2023-05-11 10:06:08 +0330

    Add painting

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ const grid = document.querySelector(".grid");
 for (let i = 1; i <= 256; i++) {
     const div = document.createElement("div");
     div.classList.add("sqr");
+    div.addEventListener("mouseover", e => {
+        if(e.buttons === 1){
+            e.target.style.backgroundColor = "red";
+        }   
+    });
     grid.append(div);
 }
