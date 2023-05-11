046c8660be671f85aa536133b4f181fae4d22418
M.Ehsan <124778596+ehsan-saf@users.noreply.github.com>
2023-05-11 23:56:46 +0330

    Calculate square dimention

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const grid = document.querySelector(".grid");
+const gridDimention = 640;
 
-for (let i = 1; i <= 256; i++) {
-    const div = document.createElement("div");
-    div.classList.add("sqr");
-    div.addEventListener("mouseover", drawColor);
-    grid.append(div);
+createSquares(50);
+
+function createSquares(quantity) {
+    const size = (gridDimention / quantity) + "px";
+    for (let i = 1; i <= quantity ** 2; i++) {
+        const div = document.createElement("div");
+        div.style.width = size;
+        div.style.height = size;
+        div.addEventListener("mouseover", drawColor);
+        grid.append(div);
+    }
 }
 
 function drawColor(e) {
+    console.log("entered");
     if(e.buttons === 1){
         e.target.style.backgroundColor = "red";
     }   
 }
+
+
