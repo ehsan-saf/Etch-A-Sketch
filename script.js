68959a1c148cedfdf383a591b97a17e2f1fe19f5
M.Ehsan <124778596+ehsan-saf@users.noreply.github.com>
2023-05-12 13:12:50 +0330

    Change grid size by user

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const grid = document.querySelector(".grid");
 const colorPicker = document.getElementById("colorPicker");
+const slider = document.getElementById("sizeSlide");
+const sizeText = document.querySelector(".size");
 
 const gridDimention = 640;
 let color = "red";
@@ -8,9 +10,16 @@ colorPicker.addEventListener("change", e => {
     color = e.target.value;
 });
 
+slider.addEventListener("input", e => {
+    const size = e.target.value;
+    createSquares(size);
+    sizeText.textContent = `${size} X ${size}`;
+});
+
 createSquares(20);
 
 function createSquares(quantity) {
+    grid.innerHTML = "";
     const size = (gridDimention / quantity) + "px";
     for (let i = 1; i <= quantity ** 2; i++) {
         const div = document.createElement("div");
